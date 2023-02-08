aa84fd207448e1a0da7d665ce138ce95b52d3197
Téo Calvo <teocalvo2@gmail.com>
2023-02-08 08:20:53 -0300

    schedule

--- src/raw/contents.py
+++ src/raw/contents.py
@@ -2,6 +2,7 @@ import argparse
 import datetime
 import json
 import requests
+import time
 
 import pandas as pd
 import boto3
@@ -45,10 +46,16 @@ def process_new_until_date(date, save_type):
     firehose = boto3.client("firehose", region_name="us-east-1")
 
     params = {"page": 1, "per_page": 100, "strategy": "new"}
+
+    print("Iniciando processo...")
     while date >= date_finish:
 
-        resp = get_data(**params)
-        data = resp.json()
+        try:
+            resp = get_data(**params)
+            data = resp.json()
+        except Exception:
+            print(resp.text)
+            break
 
         if len(data) == 0:
             break
@@ -58,10 +65,17 @@ def process_new_until_date(date, save_type):
 
         elif save_type == "firehose":
             save_with_firehose(data, firehose)
+        else:
+            pass
 
         date = pd.Timestamp(min([i["created_at"] for i in data])).date()
         print(date, params["page"])
         params["page"] += 1
+        if params["page"] % 10 == 0:
+            print("Calma, respira...")
+            time.sleep(5)
+            print("pronto, vamos la!")
+    print("Processo finalizado!")
 
 
 def main():
@@ -69,11 +83,10 @@ def main():
     default_date = datetime.datetime.now() + datetime.timedelta(days=-7)
     parser = argparse.ArgumentParser()
     parser.add_argument("--date", type=str, default=default_date, help="Data limite para busca (mais antiga): YYYY-MM-DD")
-    parser.add_argument("--save_type", type=str, default="firehose", choices=["firehose", "spark"], help="Modo de salvar os dados")
+    parser.add_argument("--save_type", type=str, default="", choices=["firehose", "spark", ""], help="Modo de salvar os dados")
     args = parser.parse_args()
 
     process_new_until_date(args.date, args.save_type)
-
 
 if __name__ == "__main__":
     main()
---
+++ src/raw/schedules.py
@@ -0,0 +1,52 @@
+import argparse
+import datetime
+import time
+
+import schedule
+
+import contents
+
+
+def execute_last_days(days, save_type):
+    date = datetime.datetime.now() + datetime.timedelta(days=-days)
+    contents.process_new_until_date(date, save_type)
+
+
+def execute_history(save_type):
+    contents.process_new_until_date("2022-01-01", save_type)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--save_type",
+        default="",
+        choices=["firehose", "spark", ""],
+        help="Modo de savar os dados coletados.",
+    )
+    parser.add_argument(
+        "--delay", default=7, help="Tamanho do delay em dias para janela de coleta."
+    )
+    parser.add_argument(
+        "--interval", default=30, help="Intervalo de tempo para nova coleta de dados."
+    )
+    parser.add_argument(
+        "--history", action="store_true", help="Flag para agendar full-load tambem."
+    )
+
+    args = parser.parse_args()
+
+    schedule.every(args.interval).minutes.do(
+        execute_last_days, args.delay, args.save_type
+    )
+
+    if args.history:
+        schedule.every(1).day.do(execute_history, args.save_type)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
